20625f457543f01c7cd635dfaaf480253ac43121
bobbyyW <bobbywang0@gmail.com>
2017-03-29 21:55:27 -0700

    add new screen. TODO add acutal info. more buttons. Just duplicate

--- ScienceProjectOxygen.py
+++ ScienceProjectOxygen.py
@@ -1,42 +1,49 @@
 # The science Project
-
-
+import tkinter as tk
 from tkinter import *
 import random
 import math
 import time
 
+#starting off
+
 root = Tk()
 root.title('Oxygen')
 root.geometry('700x700')
-canvas = Canvas(root,height = 600, width = 600, bg = 'white')
-canvas.pack()
+main = Canvas(root,height = 600, width = 600, bg = 'white')
+main.pack()
 
-#Photo
+#main
 
 oxygen = PhotoImage(file = 'Oxygen.gif')
-picture = canvas.create_image(300,300, image = oxygen)
-canvas.move(0,0,picture)
+picture = main.create_image(300,300, image = oxygen)
+main.move(0,0,picture)
 
-#canvas.move(0,0,oxygen)
+#label for main screen
 
 oxygenWord = Label(root,text = 'Oxygen', font = ('Times New Roman',30))
 oxygenWord.pack()
 
+#QUIT BUTTON
 
-#BUTTONS
+def BIB_quit():
+    pressedBib.destroy()
 
-#BASIC INFO
 def pressedBI():
     print('')
 
 BIbutton = Button(root,text = 'Basic Information', command = pressedBI)
 BIbutton.pack()
 
-#Bibliography
-
 def pressedBib():
-    print('')
+    BIBwindow = tk.Toplevel()
+    bibText = Label(BIBwindow,text = 'Oxygen is c00l', font = ('Times New Roman',30))
+    bibText.pack()
+    quitButton = Button(BIBwindow, text = 'Quit', width = 25, command = BIB_quit)
+    quitButton.pack()
 
 BibButton = Button(root, text = 'Bibliography', command = pressedBib)
 BibButton.pack()
+
+
+
